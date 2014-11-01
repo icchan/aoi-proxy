9bcc2db35078ffe40f0e64077fa12c9b2ba01d12
ian.chen <ian.chen@mail.rakuten.com>
2014-11-01 13:36:34 +0900

    tidying up of logging

--- rproxy.go
+++ rproxy.go
@@ -15,12 +15,13 @@ const (
 	GREEN = "green"
 )
 
+// Handler for admin api
 type AdminHandler struct {
 	Target *BlueGreenHandler
 }
 
 func (ah *AdminHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	log.Printf("[Admin] serving %s env: %s\n", req.URL, ah.Target.Environment)
+	log.Printf("[Admin] serving %s", req.URL)
 
 	fmt.Fprintf(w, "I am the admin api! ENV is %s\n", ah.Target.Environment)
 
@@ -35,6 +36,7 @@ func (ah *AdminHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	log.Printf("[Admin] switched to: %s\n", ah.Target.Environment)
 }
 
+// Handler for blue green switcher
 type BlueGreenHandler struct {
 	Blue        *httputil.ReverseProxy
 	Green       *httputil.ReverseProxy
@@ -54,23 +56,22 @@ func makeBlueGreen(blueString string, greenString string) *BlueGreenHandler {
 }
 
 func (bgh *BlueGreenHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	log.Println("[BlueGreen] serving ", req.URL, " from: ", bgh.Environment)
+	log.Printf("[Main]  serving from %s: %s", bgh.Environment, req.URL.Path)
 
 	if bgh.Environment == BLUE {
 		bgh.Blue.ServeHTTP(w, req)
-		//w.Write([]byte("I am Blue!!"))
 	} else {
 		bgh.Green.ServeHTTP(w, req)
-		//w.Write([]byte("I am Green!!"))
 	}
 }
 
+// Handler for alternate requests (s-out environment)
 type InverseHandler struct {
 	Target *BlueGreenHandler
 }
 
 func (ih *InverseHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	log.Println("[Inverse] serving ", req.URL, " from: NOT ", ih.Target.Environment)
+	log.Printf("[Alt]   serving from NOT %s: %s", ih.Target.Environment, req.URL.Path)
 
 	if ih.Target.Environment == BLUE {
 		ih.Target.Green.ServeHTTP(w, req)
@@ -81,8 +82,13 @@ func (ih *InverseHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 	// command line flags
-	blueBackend := flag.String("blue", "", "url to blue environment")
-	greenBackend := flag.String("green", "", "url to green environment")
+	blueBackend := flag.String("blue", "http://localhost", "url to blue environment")
+	greenBackend := flag.String("green", "http://localhost", "url to green environment")
+
+	blueGreenPort := flag.String("main", ":8080", "listen address for main http handler")
+	inversePort := flag.String("alt", ":8181", "listen address for alternate/test http handler")
+	adminPort := flag.String("admin", ":5000", "listen address for admin api")
+
 	flag.Parse()
 
 	bgHandler := makeBlueGreen(*blueBackend, *greenBackend)
@@ -94,30 +100,27 @@ func main() {
 	// Start the main handler
 	wg.Add(1)
 	go func() {
-		blueGreenPort := ":5051"
-		log.Println("[BlueGreen] Started listening on ", blueGreenPort)
+		log.Println("[Main]  Started listening on", *blueGreenPort)
 
-		log.Fatal(http.ListenAndServe(blueGreenPort, bgHandler))
+		log.Fatal(http.ListenAndServe(*blueGreenPort, bgHandler))
 		wg.Done()
 	}()
 
 	// Start the inverse handler
 	wg.Add(1)
 	go func() {
-		inversePort := ":5150"
-		log.Println("[Inverse] Started listening on ", inversePort)
+		log.Println("[Alt]   Started listening on", *inversePort)
 
-		log.Fatal(http.ListenAndServe(inversePort, &testHandler))
+		log.Fatal(http.ListenAndServe(*inversePort, &testHandler))
 		wg.Done()
 	}()
 
 	// Start the admin handler
 	wg.Add(1)
 	go func() {
-		adminPort := ":5555"
-		log.Println("[Admin] Started listening on ", adminPort)
+		log.Println("[Admin] Started listening on", *adminPort)
 
-		log.Fatal(http.ListenAndServe(adminPort, &adminHandler))
+		log.Fatal(http.ListenAndServe(*adminPort, &adminHandler))
 		wg.Done()
 	}()
 
